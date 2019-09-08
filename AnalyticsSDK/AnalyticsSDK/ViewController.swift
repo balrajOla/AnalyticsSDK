01b5ddaa6c27e936b775dd14706f77c858d394ea
Balraj Singh <erbalrajs@gmail.com>
2019-09-08 12:44:57 +0530

    Added analytics framework and ios sample to use it

---
+++ AnalyticsSDK/AnalyticsSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnalyticsSDK
+//
+//  Created by Balraj Singh on 08/09/19.
+//  Copyright © 2019 Balraj Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnalyticsSDKFramework/AnalyticsSDKFramework/AnalyticsSDKFramework.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticsSDKFramework.h
+//  AnalyticsSDKFramework
+//
+//  Created by Balraj Singh on 08/09/19.
+//  Copyright © 2019 Balraj Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalyticsSDKFramework.
+FOUNDATION_EXPORT double AnalyticsSDKFrameworkVersionNumber;
+
+//! Project version string for AnalyticsSDKFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticsSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsSDKFramework/PublicHeader.h>
+
+
